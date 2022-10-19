54ebecdc57f7ff27d1d59a3bcd8909f4dd54b154
SeneInbaraj <113924065+SeneInbaraj@users.noreply.github.com>
2022-10-19 10:02:17 +0530

    Add files via upload

---
+++ basic Java program/src/3.String/String.java
@@ -0,0 +1,12 @@
+package DataTypeString;
+
+public class String {
+
+	public static void main(String[] args) {
+		String var=new String("Hello World");
+		System.out.println(var);
+		
+
+	}
+
+}
---
+++ basic Java program/src/5.TypeCasting/TypeCasting.java
@@ -0,0 +1,16 @@
+package DataTypeCasting;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		float f=10.532f;
+		long l=(long)f;
+		double d=f;
+		byte b=12;
+		short s=b;
+		int a=1;
+		System.out.println(l);
+
+	}
+
+}
